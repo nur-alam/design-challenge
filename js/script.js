c02802a204d71827e57a2805ef7b6bcef045cc27
nur-alam <nuralam862@gmail.com>
2019-05-05 09:39:23 +0600

    added pure js count down remaining timer with pur vanilla javascript

--- js/script.js
+++ js/script.js
@@ -1,52 +1,52 @@
-jQuery(document).ready(function($) {
+const countdown = document.querySelector('.count-down');
+// Set Launch Date (ms)
+const launchDate = new Date('May 10, 2019 10:00:00').getTime();
 
-	const countdown = document.querySelector('.count-down');
-	// Set Launch Date (ms)
-	const launchDate = new Date('May 10, 2019 10:00:00').getTime();
-	
-	// Update every second
-	const intvl = setInterval(() => {
-	  // Get todays date and time (ms)
-	  const now = new Date().getTime();
-	
-	  // Distance from now and the launch date (ms)
-	  const distance = launchDate - now;
-	
-	  // Time calculation
-	  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	  const hours = days*24 + Math.floor(
-		(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-	  );
-	  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-	
-	  // Display result
-	  countdown.innerHTML = `
-	  	<div class="hours">
-	  		<p>${hours}</p>
-	  		<span>Hours</span>
-		</div>
-		<div class="minutes">
-			<p>${mins}</p>
-			<span>Minutes</span>
-		</div>
-		<div class="seconds">
-			<p>${seconds}</p>
-			<span>Seconds</span>
-		</div>
-	  `;
-	
-	  // If launch date is reached
-	  if (distance < 0) {
-		// Stop countdown
-		clearInterval(intvl);
-		// Style and output text
-		countdown.style.color = '#17a2b8';
-		countdown.innerHTML = `
-			<h3>Launched</h3>
-		`;
-	  }
-	}, 1000);
+// Update every second
+const intvl = setInterval(() => {
+  // Get todays date and time (ms)
+  const now = new Date().getTime();
+
+  // Distance from now and the launch date (ms)
+  const distance = launchDate - now;
+
+  // Time calculation
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = days*24 + Math.floor(
+	(distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display result
+  countdown.innerHTML = `
+	  <div class="hours">
+		  <p>${hours}</p>
+		  <span>Hours</span>
+	</div>
+	<div class="minutes">
+		<p>${mins}</p>
+		<span>Minutes</span>
+	</div>
+	<div class="seconds">
+		<p>${seconds}</p>
+		<span>Seconds</span>
+	</div>
+  `;
+
+  // If launch date is reached
+  if (distance < 0) {
+	// Stop countdown
+	clearInterval(intvl);
+	// Style and output text
+	countdown.style.color = '#17a2b8';
+	countdown.innerHTML = `
+		<h3>Launched</h3>
+	`;
+  }
+}, 1000);
+
+jQuery(document).ready(function($) {
  
 	$('.slider-active').owlCarousel({
 		items: 3,
